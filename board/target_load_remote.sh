70b72235147a7615d12aa408a8fff4cb741bc0b2
korr237 <korr237i@yandex.ru>
2020-04-10 19:09:35 +0300

    Updated configs, setup scripts.

--- board/target_load_remote.sh
+++ board/target_load_remote.sh
@@ -7,8 +7,10 @@ PASSWD=$3
 cd ../../buildroot
 
 # Update kernel.
-sshpass -p $PASSWD scp -P$PORT output/images/zImage		$USER@f.opteh.ru:/boot/zImage
-sshpass -p $PASSWD scp -P$PORT output/images/boot.scr	$USER@f.opteh.ru:/boot/boot.scr
+rsync -avzh -e "sshpass -p $PASSWD ssh -p $PORT" output/images/zImage	$USER@f.opteh.ru:/boot/
+rsync -avzh -e "sshpass -p $PASSWD ssh -p $PORT" output/images/boot.scr	$USER@f.opteh.ru:/boot/
+# sshpass -p $PASSWD scp -P$PORT output/images/zImage		$USER@f.opteh.ru:/boot/zImage
+# sshpass -p $PASSWD scp -P$PORT output/images/boot.scr	$USER@f.opteh.ru:/boot/boot.scr
 
 # Update filesystem.
 tar -C ../tmp/ -xf output/images/rootfs.tar

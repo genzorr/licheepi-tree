0199de5a5b0bb4e9de8de850189a96a0058936a9
korr237 <korr237i@yandex.ru>
2020-08-09 14:52:21 +0300

    Added qt5.

--- board/target_load_remote.sh
+++ board/target_load_remote.sh
@@ -7,11 +7,11 @@ PASSWD=$3
 cd ../../buildroot
 
 # Update kernel.
-rsync -avzh -e "sshpass -p $PASSWD ssh -p $PORT" output/images/zImage	$USER@f.opteh.ru:/boot/
-rsync -avzh -e "sshpass -p $PASSWD ssh -p $PORT" output/images/boot.scr	$USER@f.opteh.ru:/boot/
+rsync -aXvzh -e "sshpass -p $PASSWD ssh -p $PORT" output/images/zImage	$USER@f.opteh.ru:/boot/
+rsync -aXvzh -e "sshpass -p $PASSWD ssh -p $PORT" output/images/boot.scr	$USER@f.opteh.ru:/boot/
 # sshpass -p $PASSWD scp -P$PORT output/images/zImage		$USER@f.opteh.ru:/boot/zImage
 # sshpass -p $PASSWD scp -P$PORT output/images/boot.scr	$USER@f.opteh.ru:/boot/boot.scr
 
 # Update filesystem.
 tar -C ../tmp/ -xf output/images/rootfs.tar
-rsync -ravzh -e "sshpass -p $PASSWD ssh -p $PORT" ../tmp/ $USER@f.opteh.ru:/
+rsync -raXvzh --exclude=../tmp/usr/bin/ssh --exclude=../tmp/usr/bin/rsync -e "sshpass -p $PASSWD ssh -p $PORT" ../tmp/ $USER@f.opteh.ru:/

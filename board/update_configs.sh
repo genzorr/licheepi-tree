0394cd10ce93d2e6eeebdcbf39a97696980b5bff
korr237 <korr237i@yandex.ru>
2020-10-16 01:45:35 +0300

    (+) - Support for WiFi, FB, TFT screens, etc.     - Patches for kernel bug (yylloc in dtc package) and python2 usage in uboot. (~) - Configs update script.

--- board/update_configs.sh
+++ board/update_configs.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/bash
-cd ../
-# cp ../buildroot/.config configs/licheepi.config
-cd ../buildroot
+cd ../../buildroot
 make savedefconfig
-cd ../licheepi-tree/
 
-cp ../buildroot/output/build/linux-zero-4.13.y/.config board/linux.config
+cd ../licheepi-tree/
+cp ../buildroot/output/build/linux-*/.config board/linux.config
